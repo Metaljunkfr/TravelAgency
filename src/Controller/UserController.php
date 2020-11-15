1a7cc0e34975e3e30f07f05e8f4e72e8af6ab662
Guillaume Georges <metaljunk@hotmail.fr>
2020-11-15 08:56:23 +0100

    fix webpack

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,13 +9,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends AbstractController
 {
     /**
      * @Route("/inscription", name="inscription")
      */
-    public function index(HttpFoundationRequest $request, EntityManagerInterface $manager): Response
+    public function index(HttpFoundationRequest $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder): Response
     {
         $user = new User();
 
@@ -24,6 +25,10 @@ class UserController extends AbstractController
         $userForm->handleRequest($request);
 
         if ($userForm->isSubmitted() && $userForm->isValid()) {
+
+            $hash = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($hash);
+
             $manager->persist($user);
             $manager->flush();
 
